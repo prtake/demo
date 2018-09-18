620bf3547ab953997dc843d6c0c3528763f9e45b
yujiabao <770117176@qq.com>
2018-09-18 23:57:29 +0800

    实现视频上传

--- admin/function/del.php
+++ admin/function/del.php
@@ -10,13 +10,14 @@ require '../common/function.php';
 //接收get传值
 $times = $_GET['times'] ? $_GET['times'] : '';
 //编写查询语句
-$sql = "select id from video where uploadtime=:times";
+$sql = "select id,vdpath from video where uploadtime=:times";
 $res = $pdo->prepare($sql);
 $res->execute(array(':times' => $times));
 //返回结果
 $result = $res->fetch(PDO::FETCH_ASSOC);
 //获取结果中的id
 $id = $result['id'];
+$path = $result['vdpath'];
 if ($id) {//有结果时
     //根据id删除数据
     $sql = 'delete  from video where id=:id';
@@ -29,6 +30,7 @@ if ($id) {//有结果时
         $res1['message'] = '删除失败';
         echo json_encode($res1);
     } else {//成功返回的信息
+        unlink($path);
         $res1['status'] = '1';
         $res1['message'] = '删除成功';
         echo json_encode($res1);
---
+++ ccc.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yujiabao
+ * Date: 2018/9/18
+ * Time: 21:55
+ */
+header("Content-type:text/html;charset=utf-8");
+require 'aaa.html';
